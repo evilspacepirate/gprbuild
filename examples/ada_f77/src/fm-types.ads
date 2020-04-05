f8b376a419f46bbad694b1c0d4e46e774bc9d9f2
Nicolas Boulenguez <nicolas.boulenguez@free.fr>
2020-04-05 17:05:42 +0200

    Update GPL version in Fortran examples

--- examples/ada_f77/src/fm-types.ads
+++ examples/ada_f77/src/fm-types.ads
@@ -10,16 +10,15 @@
 --                                                                          --
 --           Copyright (C) 1995-1998 Ada Core Technologies, Inc.            --
 --                                                                          --
--- GNAT is free software;  you can  redistribute it  and/or modify it under --
--- terms of the  GNU General Public License as published  by the Free Soft- --
--- ware  Foundation;  either version 2,  or (at your option) any later ver- --
--- sion.  GNAT is distributed in the hope that it will be useful, but WITH- --
--- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
--- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
--- for  more details.  You should have  received  a copy of the GNU General --
--- Public License  distributed with GNAT;  see file COPYING.  If not, write --
--- to  the Free Software Foundation,  59 Temple Place - Suite 330,  Boston, --
--- MA 02111-1307, USA.                                                      --
+-- This is  free  software;  you can redistribute it and/or modify it under --
+-- terms of the  GNU  General Public License as published by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  This software is distributed in the hope  that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License for more details.  You should have received  a copy of the  GNU  --
+-- General Public License distributed with GNAT; see file  COPYING. If not, --
+-- see <http://www.gnu.org/licenses/>.                                      --
 --                                                                          --
 -- As a special exception,  if other files  instantiate  generics from this --
 -- unit, or you link  this unit with other files  to produce an executable, --
@@ -100,7 +99,3 @@ package FM.types is
     function To_Addr is new Unchecked_Conversion (Fortran_Subr6, Address);
     function To_Ptr6 is new Unchecked_Conversion (Address, Fortran_Subr6);
 end FM.Types;
-
-
-
-
